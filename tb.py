706471b6935d998b5ee08a15dc1e08e124fa4198
allolgeil <2806354774@qq.com>
2017-08-17 17:24:29 +0800

    Update tb.py

--- tb.py
+++ tb.py
@@ -11,4 +11,26 @@ class Tbpider(scrapy.Spider):
   start_urls = ('https://www.taobao.com/')
   
   def parse(self,response):
-    key = ''
+    key = '图书'
+    for i in range(0,10):
+      url = ''#补充内容
+      yield Request(url=url,callback=self.page)
+  
+  def page(self,response):
+    body = response.body.decode('utf-8','ignore')
+    patid = ''#补充内容
+    allid = re.compile(patid).findall(body)
+    print(allid)
+    for j in range(0,len(allid)):
+      thisid = allid[j]
+      url1 = ""#补充内容
+      yield Request(url=url,callback=self.next)
+      
+  def next(self,response):
+    item = ShopItem()
+    item['title'] = response.xpath("")#补充内容
+    item['link'] = response.url
+    item['price']= response.xpath('')#补充内容
+    '''
+    '''
+    yield item
