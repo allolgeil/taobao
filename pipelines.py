2299bee5c1753d90d33adfda4f30e29a402b3f82
allolgeil <2806354774@qq.com>
2017-08-18 11:07:39 +0800

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -2,3 +2,22 @@
 import pymysql
 
 class ShopPipeline(object):
+  def __init__(self):
+    self.conn = pymysql.connect(host='127.0.0.1',user='root',passwd='root',db='tb')
+  def process_item(self,item,spider):
+    try:
+      title = item['title'][0]
+      link = item['link']
+      price = item['price'][0]
+      comment = item['comment'][0]
+      sql = "insert into goods(title,link,price,comment)values('"+title+"','"+link+"','"+price+"','"+comment+"')"
+      self.conn.query(sql)
+      print(title)
+      print(link)
+      print(price)
+      print(comment)
+      return item
+    except Exception as e:
+      pass
+  def close_spider(self):
+    self.conn.close()
